24b1c5be6e77c42640820a2d2a9a4a9abdeeb5a3
Tim Bauer <trbauer@gmail.com>
2019-04-17 16:27:45 -0700

    improvements to RunBin.hs

--- tools/RunBin.hs
+++ tools/RunBin.hs
@@ -9,6 +9,8 @@ import NVT.Parsers.InstParser
 
 
 import Control.Monad
+import Control.Concurrent(forkIO)
+import Control.Concurrent.MVar
 import Data.List
 import Data.Bits
 import Data.Word
@@ -76,8 +78,8 @@ fatal msg = do
   exitFailure
 
 driver_skeleton :: FilePath
--- driver_skeleton = "micro_skeleton.exe"
-driver_skeleton = "micro_skeleton.cubin"
+driver_skeleton = "it/micro.exe"
+-- driver_skeleton = "micro_skeleton.cubin"
 
 runWithOpts :: Opts -> IO ()
 runWithOpts os = do
@@ -85,7 +87,23 @@ runWithOpts os = do
   (ec,out,err) <- readProcessWithExitCode exe [] ""
   case ec of
     ExitFailure ec -> fatal ("micro exited " ++ show ec ++ "\n" ++ out ++ err)
-    ExitSuccess -> return ()
+    ExitSuccess -> do
+      putStr out
+      return ()
+
+data SkeletonExeInfo =
+  SkeletonExeInfo !Int !Int !BS.ByteString
+
+getSkeletonExeInfo :: MVar SkeletonExeInfo -> IO ()
+getSkeletonExeInfo mv = do
+  bs_exe <- BS.readFile driver_skeleton
+  case findIsaRanges bs_exe of
+    [] -> fatal "no ISA sections found"
+    (_:_:_) -> fatal "multiple ISA sections found"
+    [(isa_s,isa_e)] -> do
+      -- putStrLn $ "ISA offsets: " ++ show (isa_s,isa_e)
+      let esi = SkeletonExeInfo isa_s isa_e bs_exe
+      esi `seq` putMVar mv esi
 
 
 assembleExe :: Opts -> IO FilePath
@@ -93,33 +111,41 @@ assembleExe os = do
   z <- doesFileExist patched_exe
   when z $
     removeFile patched_exe
-  inp_w128s <- assembleInput os :: IO [Word128]
+  --
+  -- start the search for the exe location in parallel to parsing and encoding
+  mv_esi <- newEmptyMVar
+  forkIO (getSkeletonExeInfo mv_esi)
+  --
+  -- start the search through the exe while we parse and assemble the input
+  inp_w128s <- assembleInput os
   let inp_bs = BS.concat $ map toByteStringU128LE inp_w128s
-  bs <- BS.readFile driver_skeleton
-  (isa_s,isa_e) <- findIsa bs
-  putStrLn $ "ISA offsets: " ++ show (isa_s,isa_e)
+  BS.length inp_bs `seq` return ()
+  putStrLn "parse/encode done"
+  --
+  -- stall until exe search done
+  SkeletonExeInfo isa_s isa_e bs_exe <- takeMVar mv_esi
   let free_space = isa_e - isa_s - BS.length end_seq
       nop_padding = free_space - BS.length inp_bs
   when (nop_padding < 0) $
-    fail "assembled ISA too large for executable"
+    fatal "assembled ISA too large for executable"
   when (nop_padding `mod` 16 /= 0) $
-    fail "slack space must be a multiple of 16"
+    fatal "slack space must be a multiple of 16"
   --  NOP   {Y};           /* 000FC00000000000`0000000000007918 */
   let nop_bs = toByteStringU128LE (Word128 0x000FC00000000000 0x0000000000007918)
   let new_isa =
         BS.concat [
-            BS.take isa_s bs
+            BS.take isa_s bs_exe
           , inp_bs
           , end_seq
           , BS.concat $ replicate (nop_padding`div`16) nop_bs -- clobber the rest with NOPs
-          , BS.drop isa_e bs
+          , BS.drop isa_e bs_exe
           ]
   BS.writeFile patched_exe new_isa
   return patched_exe
 
 patched_exe :: FilePath
--- patched_exe = "patched.exe"
-patched_exe = "patched.cubin"
+patched_exe = "micro_patched.exe"
+-- patched_exe = "patched.cubin"
 
 
 assembleInput :: Opts -> IO [Word128]
@@ -143,20 +169,56 @@ assembleInput os = do
               emitWarnings ws
               return w128s
 
+-- We find the ISA range by look for the first S2R;
+-- we find the end by walking in increments of 16 until we
+-- have the EXIT followed by BRA loop and NOPS; that is,
+-- we look for:
+--       ???
+--       S2R R0, SR_CTAID.X   000e220000002500'0000000000007919
+--       ... body
+--       ... body
+--       ... body
+--       EXIT   {!5};
+-- .L_1: BRA    `(.L_1) {Y};...
+--       NOP   {Y};
+--       ???
+--
+-- The offsets include the end sequence as part of the space we
+-- get to clobber (since we intend to replicate that higher up)
+findIsaRanges :: BS.ByteString -> [(Int,Int)]
+findIsaRanges bs = findStartOffsets 0 bs
+  where findStartOffsets :: Int -> BS.ByteString -> [(Int,Int)]
+        findStartOffsets s_off bs
+          | BS.length bs < 16 = []
+          | is_s2r_inst = findEndOffset (s_off+16) (BS.drop 16 bs)
+          | otherwise = findStartOffsets (s_off+1) (BS.drop 1 bs)
+          where is_s2r_inst = lo64_matches && hi64_matches
+                  where -- don't care about the DST register, but I do care about the SRC (I think [79:72])
+                        -- don't care about the alloc wr barrier [115:113]
+                        lo64_matches =
+                            cmpMasked
+                              mask_out_dst
+                              (fromByteStringU64LE (BS.take 8 bs))
+                              (wLo64 s2r_ctaid)
+                          where mask_out_dst = complement 0x0000000000FF0000
+                        hi64_matches =
+                            cmpMasked
+                              mask_out_wrallocbid
+                              (fromByteStringU64LE (BS.take 8 (BS.drop 8 bs)))
+                              (wHi64 s2r_ctaid)
+                          where mask_out_wrallocbid = complement 0x000E000000000000
+                        s2r_ctaid = Word128 0x000e220000002500 0x0000000000007919
+                        cmpMasked mask x y = (x .&. mask) == (y .&. mask)
 
-findIsa :: BS.ByteString -> IO (Int,Int)
-findIsa bs = findStart 0 bs
-  where findStart :: Int -> BS.ByteString -> IO (Int,Int)
-        findStart s_off bs
-          | BS.null bs = fail "unable to find start of ISA"
-          | is_s2r_inst = findEnd (s_off+16) (BS.drop 16 bs)
-          | otherwise = findStart (s_off+1) (BS.drop 1 bs)
-          where s2r_mask_out_dst = complement 0x0000000000FF0000
-                is_s2r_inst = fromByteStringU64LE (BS.take 8 bs) .&. s2r_mask_out_dst == 0x0000000000007919
-                findEnd e_off bs
-                  | BS.null bs = fail "unable to find end of ISA"
-                  | BS.take (BS.length end_seq) bs == end_seq = return (s_off,e_off+BS.length end_seq)
-                  | otherwise = findEnd (e_off + 16) (BS.drop 16 bs)
+                findEndOffset :: Int -> BS.ByteString -> [(Int,Int)]
+                findEndOffset e_off bs
+                  | BS.length bs < 16 = []
+                  | BS.take (BS.length end_seq) bs == end_seq =
+                    (s_off,e_off+end_seq_len):
+                      findStartOffsets (e_off+end_seq_len) (BS.drop end_seq_len bs)
+                  | otherwise = findEndOffset (e_off + 16) (BS.drop 16 bs)
+                  where end_seq_len = BS.length end_seq
+
 
 end_seq :: BS.ByteString
 end_seq = foldl' BS.append BS.empty (map toByteStringU64LE end_ws)
