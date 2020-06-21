b21463b04ab88bcaea1e934a28b32f6c861e2642
Tim Bauer <trbauer@gmail.com>
2020-06-21 14:33:10 -0700

    use vs2019 by default

--- tools/setup-env.bat
+++ tools/setup-env.bat
@@ -1,3 +1,4 @@
 @REM "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsall.bat" x64
+@REM "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsall.bat" x64
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvarsall.bat" x64
 set PATH=%PATH%;%CUDA_PATH%\bin
