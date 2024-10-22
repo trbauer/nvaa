11e0925b638fafee1b7fac6d41f65bc0f78e71ea
Tim <trbauer@gmail.com>
2024-10-21 23:56:03 -0500

    [exp] more changes for const

--- experiments/consts/const-rlat.cu
+++ experiments/consts/const-rlat.cu
@@ -93,36 +93,42 @@ static __global__ void ffma_c(
 }
 
 
-int main(int argc, char **argv)
+struct test {
+  enum class ordinal {R, C} code;
+  const char *symbol;
+  explicit test (ordinal c, const char *sym) : code(c), symbol(sym) { }
+};
+static const test R {test::ordinal::R, "r"};
+static const test C {test::ordinal::C, "c"};
+static const test *all_tests[] {&R, &C};
+
+static void run_test(test t, int itrs, int verbosity)
 {
-  umem<uint64_t> out_times[] {
-    umem<uint64_t>{1, const_seq<uint64_t>(0)},
-    umem<uint64_t>{1, const_seq<uint64_t>(0)},
-    umem<uint64_t>{1, const_seq<uint64_t>(0)},
-  };
   umem<float> oups(1, const_seq<float>(0.0f));
-  umem<float4> inps((size_t)1, arith_seq<float4>());
-  std::vector<float> elapsed_ss;
-  std::string arg = argv[1];
-  if (argc != 2) {
-    fatal("expected 'c' or 'r' as argument");
+  umem<float4> inps(1, arith_seq<float4>());
+  std::vector<umem<uint64_t>> out_times;
+  out_times.reserve(itrs);
+  std::vector<std::function<void()>> funcs;
+  funcs.reserve(itrs);
+  //
+  for (int i = 0; i < itrs; i++) {
+    out_times.emplace_back(1, const_seq<uint64_t>(0));
+    if (t.code == test::ordinal::R) {
+      funcs.push_back([&]{
+        std::cout << "calling ffma_r\n";
+        std::cout << &oups << "\n";
+        std::cout << &inps << "\n";
+        std::cout << &out_times[i] << "\n";
+        ffma_r<<<1,1>>>(out_times[i], oups, inps);
+        std::cout << "called ffma_r\n";
+        });
+    } else if (t.code == test::ordinal::C) {
+      funcs.push_back([&]{ffma_c<<<1,1>>>(out_times[i], oups, inps, 0.5f);});
+    } else {
+      fatal("unreachable");
+    }
   }
-  if (arg == "r") {
-    elapsed_ss = time_dispatches_s({
-      [&] {ffma_r<<<1,1>>>(out_times[0], oups, inps);},
-      [&] {ffma_r<<<1,1>>>(out_times[1], oups, inps);},
-      [&] {ffma_r<<<1,1>>>(out_times[2], oups, inps);},
-      });
-  } else if (arg == "c") {
-    elapsed_ss = time_dispatches_s(
-      {
-      [&] {ffma_c<<<1,1>>>(out_times[0], oups, inps, 0.5f);},
-      [&] {ffma_c<<<1,1>>>(out_times[1], oups, inps, 0.5f);},
-      [&] {ffma_c<<<1,1>>>(out_times[2], oups, inps, 0.5f);},
-      });
-  } else {
-    fatal("expected 'c' or 'r' as argument");
-  }
+  std::vector<float> elapsed_ss = time_dispatches_s(funcs);
   auto e0 = cudaDeviceSynchronize();
   if (e0 != cudaSuccess) {
     fatal(cudaGetErrorName(e0), " (", cudaGetErrorString(e0), "): unexpected error");
@@ -131,5 +137,75 @@ int main(int argc, char **argv)
     std::cout << format("elapsed_s[", i, "]: ",
                         frac(elapsed_ss[i], 3), ", ", (int64_t)out_times[i][0], " c\n");
   }
+}
+
+int main(int argc, char **argv)
+{
+  if (argc == 1) {
+    fatal("use -h for help");
+  }
+  const int dft_itrs = 3;
+  int itrs = dft_itrs;
+  int verbosity = 0;
+  std::vector<test> tests;
+  for (int ai = 1; ai < argc; ai++) {
+    std::string arg = argv[ai];
+    std::string key = arg, val;
+    auto eq = arg.find('=');
+    if (eq != std::string::npos) {
+      key = arg.substr(0, eq + 1); // include the =
+      val = arg.substr(eq + 1);
+    }
+    auto bad_opt = [&](const char *msg) {
+      fatal(arg, ": ", msg);
+    };
+
+    if (arg == "-h" || arg == "--help") {
+      std::stringstream uss;
+      uss <<
+        "usage: " << argv[0] << " [OPTS] TESTS\n"
+        "where [OPTS]:\n"
+        "  -i/--iterations=INT   number of runs to take the min of\n"
+        "                        (defaults to " << dft_itrs << ")\n"
+        "  -v/-v2/-v3            verbosity/debug\n"
+        "and TESTS are:\n"
+        "          a       // runs all tests\n";
+      for (const auto *t : all_tests) {
+        uss << "        | " << t->symbol << "\n";
+      }
+      std::cout << uss.str();
+      exit(EXIT_SUCCESS);
+    } else if (key == "-i=") {
+      itrs = parse_integral_positive<int>(val, false);
+    } else if (arg == "-v") {
+      verbosity = 1;
+    } else if (arg == "-v2") {
+      verbosity = 2;
+    } else if (arg == "-v3") {
+      verbosity = 3;
+    } else if (arg.substr(0, 1) == "-") {
+      bad_opt("invalid option");
+    } else if (arg == "a") {
+      for (const test *t : all_tests) {
+        tests.push_back(*t);
+      }
+    } else {
+      bool found = false;
+      for (const test *t : all_tests) {
+        if (arg == t->symbol) {
+          tests.push_back(*t);
+          found = true;
+          break;
+        }
+      }
+      if (!found) {
+        bad_opt("invalid test name");
+      }
+    }
+  } // for
+  for (test t : tests) {
+    run_test(t, itrs, verbosity);
+  }
+
   return EXIT_SUCCESS;
 }
